d11d9723506e882c1f180bccbe6d512824eaf34a
Andrew <andrewlagreca2@gmail.com>
2026-08-14 21:23:29 -0700

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.ts
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        let map = new Map();
+        let answer = []
+
+        for(const num of nums) {
+            if(!map.has(num)) {
+                map.set(num, 1)
+            } else {
+                map.set(num, map.get(num) + 1)
+            }
+        }
+
+        for(let i = 0; i < k; i++) {
+            let greatest = -1000;
+            let gKey = 0
+
+            for(const num of nums) {
+                if(map.get(num) > greatest) { 
+                    greatest = map.get(num)
+                    gKey = num
+                }
+            }
+
+            map.delete(gKey)
+            answer.push(gKey)
+        }
+
+        return answer
+    }
+}
