b846bcd7d1cecea2d8c8ac907bf038ae5b959e51
Andrew <andrewlagreca2@gmail.com>
2026-08-20 22:35:09 -0700

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        let map = new Map<string, string[]>()
+
+        for(let i = 0; i < strs.length; i++) {
+            let current = strs[i].split("").sort().join()
+
+            if(!map.has(current)) {
+                map.set(current, [])
+            }
+
+            map.get(current).push(strs[i])
+        }
+
+        return Array.from(map.values())
+    }
+}
